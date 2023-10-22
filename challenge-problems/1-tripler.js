d8eb14972c1dd56dee75b2e5f723da0fc2bd22d1
Ellie Solhjou <elliesolhjou@gmail.com>
2023-10-22 11:03:15 -0500

    Complete 1-tripler Challenge

--- challenge-problems/1-tripler.js
+++ challenge-problems/1-tripler.js
@@ -9,8 +9,12 @@ tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
 function tripler(array) {
-    // your code here
+    let newArray=[]
+    for (let i=0; i<array.length; i++){
+        newArray.push(array[i]*=3)
+    }
+    return newArray
 }
-
+console.log(tripler([1,2,3]))
 
 module.exports = tripler
